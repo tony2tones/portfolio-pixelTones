dbafe262c7a8149f244eadd566bc0f953ecc060b
Anthony <anthonymarques@outlook.com>
2020-02-10 22:03:13 +0200

    working wtih a paralax

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,21 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ExampleComponent } from './example/example.component';
-
-import simpleParallax from 'simple-parallax-js';
-import { ParallaxDirective } from './parallax.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ExampleComponent,
-    ParallaxDirective
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    simpleParallax
   ],
   providers: [],
   bootstrap: [AppComponent]
